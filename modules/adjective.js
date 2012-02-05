882625729f694945755c28f9cc0ac0f007983aaf
Kevin Goess <kevin@musgrave.local>
2012-02-05 12:39:18 +0100

    debugging cruft out

--- modules/adjective.js
+++ modules/adjective.js
@@ -1,7 +1,7 @@
 
 var exported = require('../node_modules/yui3-mocha/package.js');
 
-exported.YUI().use('nodex', function (Y){
+exported.YUI().use('node', function (Y){
 /**
  * Can be attached to the subject or the direct object, or turned off on either/both of those
  * @class Adjective
@@ -14,10 +14,6 @@ function Adjective(args){
     this.mPersMasc = args[4];
 
     this.wordId = this.m; // like a primary key for all the words here
-}
-var m;
-for(m in Y){
-    console.log("Y."+m+" is " + Y.m);
 }
 console.log("in my adjective.js Y is " + Y);
 Y.extend(Adjective, Word);
