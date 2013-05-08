1297990b9ee6d3b2db869122a7681185cc0d5f95
Kevin Goess <knet@goess.org>
2013-05-07 22:04:57 -0700

    duplicate wrapInTooltipDivsTemplate function

--- modules/word.js
+++ modules/word.js
@@ -36,21 +36,6 @@ Word.prototype.wrapInTooltipDivsTemplate = function (args){
 // looking for :hover on span instead of a tag means IE6 isn't supported, oh well
 Word.prototype.tooltipTemplate = '<span class="answer-word" >{polishStr}<span class="tooltip"><a href="http://en.wiktionary.org/wiki/{wikiLookup}#Polish" target="_blank">{tooltipStr}</a></span></span> ';
 
-Word.prototype.wrapInTooltipDivsTemplate = function (args){
-    var str              = args.polishStr;
-    var wikiLookup       = args.wikiLookup;
-    var tooltipStr       = args.tooltipStr;
-
-    var tooltipTemplate = this.tooltipTemplate;
-
-    var stash = {
-        polishStr: str,
-        wikiLookup: wikiLookup,
-        tooltipStr: tooltipStr
-    }
-
-    return Y.substitute(tooltipTemplate, stash);
-};
 
 Word.prototype.pushpinTemplate = '<span class="question-word" id="question-word-{partOfSpeech}"><span class="pushpin {hiddenClass} {onOffClass}" id="question-word-{partOfSpeech}-pushpin"><img src="{imgSrc}"/></span>{englishStr}</span>';
 
@@ -64,6 +49,9 @@ Word.prototype.wrapInPushpinDivs = function(englishStr, partOfSpeech){
         onOffClass: (this.pinned ? 'pushpin-on' : 'pushpin-off')
     };
         
+    // apparently Y.sub is deprecated, the docs say:
+    // Use Y.Lang.sub or Y.Template instead.
+    // http://yuilibrary.com/yui/docs/api/classes/YUI~substitute.html
     return Y.substitute(this.pushpinTemplate, stash);
 };
 
